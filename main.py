ef6a89531bbfafd7db07eb3adb6222eee25ef098
rogerkreienbuehl <r.kreienbuehl>
2019-02-15 20:31:47 +0100

    gpio initialization

--- main.py
+++ main.py
@@ -4,11 +4,20 @@
 from signal import SIGINT, SIGTERM, signal
 from time import sleep
 from Webserver import Webserver
+import RPi.GPIO as GPIO
 
 
 def main():
 
     global web
+
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setwarnings(False)
+
+    GPIO.setup(18, GPIO.OUT)
+    GPIO.setup(23, GPIO.OUT)
+    GPIO.setup(24, GPIO.OUT)
+    GPIO.setup(25, GPIO.OUT)
 
     signal(SIGINT, exit_gracefully)
     signal(SIGTERM, exit_gracefully)
